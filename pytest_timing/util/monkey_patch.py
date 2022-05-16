df1b13c753a0f7e44e547fefbd3ccc4569417e37
Rafe Kaplan <slobberchops@users.noreply.github.com>
2022-05-15 21:08:27 -0400

    Added patch class. Saves and restores objects in modules.

--- pytest_timing/util/monkey_patch.py
+++ pytest_timing/util/monkey_patch.py
@@ -3,6 +3,7 @@ import functools
 import importlib
 import operator
 import sys
+from typing import Any
 from typing import Callable
 from typing import Optional
 
@@ -43,4 +44,28 @@ class Spec:
         return cls(target.__module__, target.__qualname__)
 
 
-__all__ = ("Spec",)
+@dataclasses.dataclass(frozen=True)
+class Patch:
+
+    original_obj: Any
+    spec: Spec
+
+    def install(self, obj: Any):
+        module = self.spec.get_module()
+        parent_name = self.spec.parent_qualified_name
+        if parent_name:
+            parent_getter = operator.attrgetter(parent_name)
+            parent = parent_getter(module)
+        else:
+            parent = module
+        setattr(parent, self.spec.name, obj)
+
+    def restore(self):
+        self.install(self.original_obj)
+
+    @classmethod
+    def from_spec(cls, spec: Spec):
+        return cls(spec.get_obj(), spec)
+
+
+__all__ = ("Patch", "Spec")
--- tests/pytest_timing/util/test_monkey_patch.py
+++ tests/pytest_timing/util/test_monkey_patch.py
@@ -22,6 +22,21 @@ def double_nested_spec():
     return monkey_patch.Spec(target_module.__name__, "Top.Nested.DoubleNested")
 
 
+@pytest.fixture
+def top_patch(top_spec):
+    return monkey_patch.Patch.from_spec(top_spec)
+
+
+@pytest.fixture
+def nested_patch(nested_spec):
+    return monkey_patch.Patch.from_spec(nested_spec)
+
+
+@pytest.fixture
+def double_nested_patch(double_nested_spec):
+    return monkey_patch.Patch.from_spec(double_nested_spec)
+
+
 class TestSpec:
     class TestName:
         @staticmethod
@@ -73,3 +88,53 @@ class TestSpec:
             monkey_patch.Spec.from_target(target_module.Top.Nested.DoubleNested)
             == double_nested_spec
         )
+
+
+class TestPatch:
+    @staticmethod
+    def test_from_spec(top_spec, nested_spec, double_nested_spec):
+        top_patch = monkey_patch.Patch.from_spec(top_spec)
+        nested_patch = monkey_patch.Patch.from_spec(nested_spec)
+        double_nested_patch = monkey_patch.Patch.from_spec(double_nested_spec)
+
+        assert top_patch.spec is top_spec
+        assert nested_patch.spec is nested_spec
+        assert double_nested_patch.spec is double_nested_spec
+
+        assert top_patch.original_obj is target_module.Top
+        assert nested_patch.original_obj is target_module.Top.Nested
+        assert double_nested_patch.original_obj is target_module.Top.Nested.DoubleNested
+
+    class TestInstallAndRestore:
+        @staticmethod
+        def test_top(top_patch):
+            new_object = object()
+            try:
+                top_patch.install(new_object)
+                assert target_module.Top is new_object
+            finally:
+                top_patch.restore()
+                assert target_module.Top is top_patch.original_obj
+
+        @staticmethod
+        def test_nested(nested_patch):
+            new_object = object()
+            try:
+                nested_patch.install(new_object)
+                assert target_module.Top.Nested is new_object
+            finally:
+                nested_patch.restore()
+                assert target_module.Top.Nested is nested_patch.original_obj
+
+        @staticmethod
+        def test_double_nested(double_nested_patch):
+            new_object = object()
+            try:
+                double_nested_patch.install(new_object)
+                assert target_module.Top.Nested.DoubleNested is new_object
+            finally:
+                double_nested_patch.restore()
+                assert (
+                    target_module.Top.Nested.DoubleNested
+                    is double_nested_patch.original_obj
+                )
