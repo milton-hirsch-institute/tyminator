0c2cdffd8e864a7c5793f3373f392fedaad0faca
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2023-06-13 18:19:18 +0100

    Removed pytest integration

--- tests/pytest_timing/conftest.py
+++ tests/pytest_timing/conftest.py
@@ -1,4 +1,34 @@
-from pytest_timing.plugin import clock  # noqa: unused-import
-from pytest_timing.plugin import clock_local_tz  # noqa: unused-import
-from pytest_timing.plugin import clock_start  # noqa: unused-import
-from pytest_timing.plugin import clock_step  # noqa: unused-import
+import datetime
+
+import pytest
+
+from pytest_timing import clock as clock_module
+from pytest_timing import defaults
+
+
+@pytest.fixture
+def clock(clock_start, clock_step, clock_local_tz) -> clock_module.Clock:
+    return clock_module.Clock(clock_start, clock_step, local_tz=clock_local_tz)
+
+
+@pytest.fixture
+def clock_start() -> datetime.datetime:
+    return defaults.DEFAULT_CLOCK_START
+
+
+@pytest.fixture
+def clock_local_tz() -> datetime.tzinfo:
+    return defaults.DEFAULT_LOCAL_TZ
+
+
+@pytest.fixture
+def clock_step() -> clock_module.Step:
+    return 1
+
+
+__all__ = (
+    "clock",
+    "clock_local_tz",
+    "clock_start",
+    "clock_step",
+)
